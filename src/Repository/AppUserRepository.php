618ba2a388eed7cb2778ccdb3c8dfa7bfacf8987
james <james.whiteman@gmail.com>
2018-07-17 16:49:40 +1200

    fist commit

--- src/Repository/AppUserRepository.php
+++ src/Repository/AppUserRepository.php
@@ -8,19 +8,9 @@
 
 namespace App\Repository;
 
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Doctrine\ORM\EntityRepository;
 
-class AppUserRepository extends EntityRepository implements UserLoaderInterface
+class AppUserRepository extends EntityRepository
 {
-    public function loadUserByUsername($username)
-    {
-        return
-            $this->createQueryBuilder('u')
-            ->where('u.username = :username OR u.email = :email')
-            ->setParameter('username', $username)
-            ->setParameter('email', $username)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
+
 }
